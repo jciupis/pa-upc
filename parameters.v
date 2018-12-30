72d1e34a3f64b0b3625fc8e292bb6dd54c370c60
jciupis <jedrzej.ciupis@gmail.com>
2018-12-30 16:46:59 +0100

    Basic execute stage

--- parameters.v
+++ parameters.v
@@ -3,14 +3,14 @@
  */
 
 /* OPCODE definitions */
-`define OP_ADD	7'b000_0000	/* ADD	0x00 */
-`define OP_SUB	7'b000_0001	/* SUB	0x01 */
-`define OP_MUL	7'b000_0010	/* MUL	0x02 */
-`define OP_LDB	7'b001_0000	/* LDB	0x10 */
-`define OP_LDW	7'b001_0001	/* LDW	0x11 */
-`define OP_STB	7'b001_0010	/* STB	0x12 */
-`define OP_STW	7'b001_0011	/* STW	0x13 */
-`define OP_BEQ	7'b011_0000	/* BEQ	0x30 */
-`define OP_JMP	7'b011_0001	/* JMP	0x31 */
+`define OP_ADD	6'b00_0000	/* ADD	0x00 */
+`define OP_SUB	6'b00_0001	/* SUB	0x01 */
+`define OP_MUL	6'b00_0010	/* MUL	0x02 */
+`define OP_LDB	6'b01_0000	/* LDB	0x10 */
+`define OP_LDW	6'b01_0001	/* LDW	0x11 */
+`define OP_STB	6'b01_0010	/* STB	0x12 */
+`define OP_STW	6'b01_0011	/* STW	0x13 */
+`define OP_BEQ	6'b11_0000	/* BEQ	0x30 */
+`define OP_JMP	6'b11_0001	/* JMP	0x31 */
 
 /* TODO */
