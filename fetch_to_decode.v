28867da0da94995d2acca82f381e90398418b11d
jciupis <jedrzej.ciupis@gmail.com>
2019-01-11 20:22:42 +0100

    Add data memory controller to the CPU

--- fetch_to_decode.v
+++ fetch_to_decode.v
@@ -6,16 +6,17 @@ module fetch_to_decode
     /* Fetch stage variables. */
     input [31:0] f_instr,      // Instruction.
     input [31:0] f_pc,         // Program counter.
-    input        f_stall,      // Stall the pipeline.
+    input        f_stall,      // Flag indicating that this stage is stalled.
     
     /* Decode stage variables. */
+    input             d_stall, // Flag indicating that this stage is stalled.
     output reg [31:0] d_instr, // Instruction.
     output reg [31:0] d_pc     // Program counter.
 );
 
     always @(posedge clock) begin
-        d_instr <= (reset) ? 32'b0 : (f_stall ? 32'b0 : f_instr);
-        d_pc    <= (reset) ? 32'b0 : f_pc;
+        d_instr <= (reset) ? 32'b0 : (d_stall ? d_instr : (f_stall ? 32'b0 : f_instr));
+        d_pc    <= (reset) ? 32'b0 : (d_stall ? d_pc                       : f_pc);
     end
 
 endmodule
